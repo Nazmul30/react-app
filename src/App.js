62a287a15ce71079049edd18ad3c18b44ab28092
Nazmul <nazmulraj18@gmail.com>
2021-03-04 16:19:57 +0600

    react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+ import Country from './components/Country/Country';
+ import Cart from './components/Cart/Cart';
 
 function App() {
+  const [countries, setCountries]= useState([]);
+  const [cart, setCart]=useState([]);
+
+  useEffect(()=>{
+      fetch('https://restcountries.eu/rest/v2/all')
+       .then(res=>res.json())
+       .then(data=> setCountries(data))
+       .catch(error=>console.log(error))
+
+       },[])
+
+       const handleAddCountry =(country)=>{
+         const newCart=[...cart, country];
+          setCart(newCart);
+       }
   return (
     <div className="App">
+      <h1> Country Loaded:{countries.length}</h1>
+       <h4>Country added:{cart.length}</h4>
+        <Cart cart={cart}>this is cart</Cart>
+     
+      <ul>
+        {countries.map(country=><Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)}
+      </ul>
+ 
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -14,8 +41,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        
           Learn React
+          >
         </a>
       </header>
     </div>
